0ab766242a49ea9a0797d557c0e0ddc723799027
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-03 16:08:47 -0400

    editing env.c

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 15:05:48 by mouhamad_kr       #+#    #+#             */
-/*   Updated: 2024/09/01 10:04:35 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/09/03 16:06:53 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_env(t_env *env_list)
 	while (temp != NULL)
 	{
 		if (temp->hidden == 0)
-			printf("%s=\"%s\"\n", temp->key, temp->value);
+			printf("%s=%s\n", temp->key, temp->value);
 		temp = temp->next;
 	}
 	return (0);
