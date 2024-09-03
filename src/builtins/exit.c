1c9d946fdb31c6ecd6b73cfd0fbf675d9858a7a4
mkraytem <moohamadkraytem15@gmail.com>
2024-09-03 22:54:41 +0300

    fix ft_exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mouhamad_kraytem <mouhamad_kraytem@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 21:27:26 by mouhamad_kr       #+#    #+#             */
-/*   Updated: 2024/09/03 14:16:54 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/09/03 22:53:38 by mouhamad_kr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,10 +95,7 @@ int check_status_str(char *str)
 
     i = 0;
     if (str[i] == '-' || str[i] == '+')
-    {
         i++;
-    }
-    
     while (str[i])
     {
         if (!ft_isdigit(str[i]))
@@ -121,38 +118,44 @@ int calculate_exit_status(int status)
 
     return res;
 }
-
-int convert_and_validate_exit_status(char *arg)
-{
-    int exit_status;
-
-    if (!check_status_str(arg))
-    {
-        printf("exit: %s: numeric argument required\n", arg);
-        return 1; // Standard behavior for non-numeric arguments
-    }
-
-    exit_status = ft_atoi(arg);
-
-    printf("exit: %d\n", calculate_exit_status(exit_status));
-    return calculate_exit_status(exit_status);
-}
-
 void handle_too_many_args_error()
 {
     printf("exit: too many arguments\n");
     exit(1);
 }
 
+int convert_and_validate_exit_status(char **arg)
+{
+    long exit_status;
+
+    if (!check_status_str(arg[1]))
+    {
+        printf("exit: %s: numeric argument required\n", arg[1]);
+        return (2); // Standard behavior for non-numeric arguments
+    }
+    if (arg[2] != NULL)
+    {
+        handle_too_many_args_error();
+        return (1);
+    }
+    
+    exit_status = ft_atol(arg[1]);
+    if (exit_status > LONG_MAX || exit_status < LONG_MIN)
+    {
+        printf("exit: %s: numeric argument required\n", arg[1]);
+        return 2; // Standard behavior for non-numeric arguments
+    }
+
+    printf("exit: %d\n", calculate_exit_status((int)exit_status));
+    return calculate_exit_status((int)exit_status);
+}
+
 int ft_exit(char **args)
 {
     int exit_status;
 
-    if (args[2] != NULL)
-        handle_too_many_args_error();
-
     if (args[1] != NULL)
-        exit_status = convert_and_validate_exit_status(args[1]);
+        exit_status = convert_and_validate_exit_status(args);
     else
         exit_status = 0; // Default exit status
 
