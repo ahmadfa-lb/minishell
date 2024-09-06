2c3285642bd03410fdb0e58dbad563a40e5a303d
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-06 14:58:24 -0400

    cleaning signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,16 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signal.c                                           :+:      :+:    :+:   */
+/*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 00:00:34 by zmourtab          #+#    #+#             */
-/*   Updated: 2024/09/03 09:18:51 by afarachi         ###   ########.fr       */
+/*   Created: 2024/09/05 12:44:16 by afarachi          #+#    #+#             */
+/*   Updated: 2024/09/06 14:57:18 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int	g_signal_number;
 
-			
+void	set_signals(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, handle_ctrl_c);
+}
+
+void	reset_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	set_execution_signals(void)
+{
+	// signal(SIGQUIT, );
+	// signal(SIGINT, );
+}
+
+void	ignore_signals(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
+}
---
+++ src/signals/signals_helpers.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals_helpers.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 12:44:16 by afarachi          #+#    #+#             */
+/*   Updated: 2024/09/06 13:08:42 by afarachi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void handle_ctrl_d(t_data *data)
+{
+    if (!data->user_input)
+    {
+		free_data(data);
+		free(data);
+		rl_clear_history();
+		printf("exit\n");
+    	exit(0);
+    }
+    else
+        printf("\n"); 
+}
+
+void	handle_ctrl_c(int sig)
+{
+	g_signal_number = sig;
+	rl_replace_line("", 1);
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void update_exit_status_on_signal(int *status)
+{
+    if (g_signal_number == SIGINT)
+    {
+        *status = 130;
+    }
+    else if (g_signal_number== SIGQUIT)
+    {
+        *status = 131;
+    }
+    g_signal_number = 0;
+}
